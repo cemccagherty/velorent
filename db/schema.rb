9d47349b9ecb47d96d76563e27b1991a51c0f9ef
Bilgin Bilir <bilgin.bilir@whu.edu>
2025-03-12 15:06:41 +0100

    Added link from bikes#index to orders#index

--- db/schema.rb
+++ db/schema.rb
@@ -19,9 +19,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_12_114013) do
     t.string "brand"
     t.integer "year"
     t.string "color"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_bikes_on_user_id"
   end
 
