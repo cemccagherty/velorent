5c43ab714894fd191bcfbaef11040ef09c148d17
Christian McCagherty <christianmccagherty164@gmail.com>
2025-03-12 11:52:03 +0000

    order now saves date param on create

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,19 +3,19 @@ class OrdersController < ApplicationController
   def create
     @bike = Bike.find(params[:bike_id])
     @user = current_user
-    @order = Order.new
+    @order = Order.new(order_params)
     @order.user = @user
     @order.bike = @bike
     if @order.save
-      redirect_to bikes_path
+      redirect_to root_path
     else
-      render bikes_path, status: :unprocessable_entity
+      redirect_to bikes_path(@bike), status: :unprocessable_entity
     end
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:bike_id, :user_id, :rental_date)
+    params.require(:order).permit(:rental_date)
   end
 end
