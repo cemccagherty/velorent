bc87fe0ab7d4aaee20d5ebcf73b4f1159edad412
oben karasaka <obenkarasaka@gmail.com>
2025-03-13 15:51:00 +0100

    username and email added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  def find_for_database_authentication(conditions)
+    login = conditions.delete(:login)
+    where(conditions).where(["email = :value OR username = :value", { value: login }]).first
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 class User < ApplicationRecord
-  has_many :orders, dependent: :destroy
-  has_one_attached :photo
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  attr_accessor :login # Allows Devise to accept 'login' as a field
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+
+  # Custom method to authenticate via either username or email
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+
+    where(conditions.to_h).where(["lower(email) = :value OR lower(username) = :value", { value: login.downcase }]).first
+  end
 end
