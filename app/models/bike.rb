1a9c356b35db1a7aa711ebd51f311a1a895ee896
Christian McCagherty <christianmccagherty164@gmail.com>
2025-03-13 13:57:28 +0000

    bike validation error messages added

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -3,6 +3,6 @@ class Bike < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
 
-  validates :bike_type, :brand, :year, :color, presence: true
+  validates :bike_type, :brand, :year, :color, presence: { message: "required field" }
   validates :year, length: { is: 4 }
 end
