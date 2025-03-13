518fb6757f9152aba76844d0811ac8bdcf9d04c7
Jose Oliveira <jedpfo89@gmail.com>
2025-03-13 16:01:01 +0000

    hmmmm

---
+++ app/javascript/controllers/search_bar_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="search-bar"
+export default class extends Controller {
+  static targets = ["search"]
+
+  connect() {
+    console.log("hello")
+  }
+
+  refresh() {
+    this.searchTarget.innerHtml = "<%= render \"bikeslist\" %>"
+  }
+}
